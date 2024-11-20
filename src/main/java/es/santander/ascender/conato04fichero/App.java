65599c4f51724412b5489620befeea8d7af20305
Juanma <Juanma888@gmail.com>
2024-11-20 17:43:01 +0100

    --

---
+++ src/main/java/es/santander/ascender/conato04fichero/App.java
@@ -0,0 +1,10 @@
+package es.santander.ascender.conato04fichero;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
